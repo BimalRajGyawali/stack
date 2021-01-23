79c9861385d6e676286185200add06e802803d76
Bimal Raj Gyawali <gyawali.rajbimal35@gmail.com>
2021-01-23 23:38:00 +0545

    adds infix-to-postfix converter

---
+++ src/usages/InfixToPostfixConverter.java
@@ -0,0 +1,68 @@
+package usages;
+
+import core.Stack;
+
+public class InfixToPostfixConverter {
+	private Stack<Character> stack;
+	private String postFixString;
+
+	public InfixToPostfixConverter() {
+		this.stack = new Stack<Character>();
+		this.postFixString = "";
+	}
+
+	public String convert(String infixExpression) {
+		char[] characters = infixExpression.toCharArray();
+		for (char character : characters) {
+			if (character == '(') {
+				this.stack.push(character);;
+			} else if (character == ')') {
+				while (this.stack.peek() != '(') {
+					this.postFixString += this.stack.pop();
+				}
+				this.stack.pop();
+			} else if (isOperator(character)) {
+				if (this.stack.isEmpty()) {
+					this.stack.push(character);
+				} else {
+
+					while ( !this.stack.isEmpty() &&
+							this.stack.peek() != '(' &&
+							!hasLesserPrecedence(this.stack.peek(), character)) {
+						this.postFixString += this.stack.pop();
+					}
+					this.stack.push(character);
+				}
+
+			} else {
+				this.postFixString += character;
+			}
+		}
+		// when the infix string is scanned, pop all contents of stack to postfix string
+		while (!this.stack.isEmpty()) {
+			this.postFixString += this.stack.pop();
+		}
+
+		return this.postFixString;
+	}
+
+	private boolean hasLesserPrecedence(char first, char second) {
+		// checks if first operator has lesser precedence
+		if ((first == '-' || first == '+') && (second == '*' || second == '/' || second == '^')) {
+			return true;
+		}
+		if ((first == '*' || first == '/') && second == '^') {
+			return true;
+		}
+
+		return false;
+	}
+
+	private boolean isOperator(char c) {
+		if (c == '+' || c == '-' || c == '*' || c == '/' || c == '^') {
+			return true;
+		}
+		return false;
+	}
+
+}
