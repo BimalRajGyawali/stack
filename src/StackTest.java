0f2dc7218c7faa6bd5b7952f642e65c6686b1a20
Bimal Raj Gyawali <gyawali.rajbimal35@gmail.com>
2021-01-24 00:40:58 +0545

    adds test for converters

--- src/StackTest.java
+++ src/StackTest.java
@@ -1,15 +1,18 @@
 
 
 import usages.InfixToPostfixConverter;
+import usages.InfixToPrefixConverter;
 
 public class StackTest {
 	public static void main(String[] args) {
 
 //		String infixString = "a+(b-c)";
 //		String infixString = "a*b+c/d-e*f";
-		String infixString = "a+b-d*(e+f)^g/h";
+		String infixString = "h/g^(f+e)*d-b+a";
 		var converter = new InfixToPostfixConverter(infixString);
 		String postfixString = converter.convert();
 		System.out.println(postfixString);
+		
+		System.out.println(new InfixToPrefixConverter(infixString).convert());
 	}
 }
