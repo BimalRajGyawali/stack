00d68e8956c510f48457ad9566dcda77f7600976
Bimal Raj Gyawali <gyawali.rajbimal35@gmail.com>
2021-01-23 23:45:12 +0545

    modifies method call

--- src/StackTest.java
+++ src/StackTest.java
@@ -5,11 +5,11 @@ import usages.InfixToPostfixConverter;
 public class StackTest {
 	public static void main(String[] args) {
 
-		var converter = new InfixToPostfixConverter();
 //		String infixString = "a+(b-c)";
 //		String infixString = "a*b+c/d-e*f";
 		String infixString = "a+b-d*(e+f)^g/h";
-		String postfixString = converter.convert(infixString);
+		var converter = new InfixToPostfixConverter(infixString);
+		String postfixString = converter.convert();
 		System.out.println(postfixString);
 	}
 }
