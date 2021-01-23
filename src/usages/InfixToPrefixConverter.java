d6f637c0d229fd36bb325c3272548198d1a0f12b
Bimal Raj Gyawali <gyawali.rajbimal35@gmail.com>
2021-01-24 00:40:40 +0545

    adds infix-to-prefix converter

--- src/usages/InfixToPrefixConverter.java
+++ src/usages/InfixToPrefixConverter.java
@@ -8,8 +8,20 @@ public class InfixToPrefixConverter {
 	}
 	
 	public String convert() {
-		String reversedInfixString = new StringBuilder(this.infixString).reverse().toString();
-		String postfixString = new InfixToPrefixConverter(reversedInfixString).convert();
+		String reversedInfixString = "";
+		
+		char[] characters = this.infixString.toCharArray();
+		for(int i=characters.length-1; i>=0; i--) {
+			char c = characters[i];
+			if(c == '(') {
+				reversedInfixString += ')';
+			}else if(c == ')') {
+				reversedInfixString += '(';
+			}else {
+				reversedInfixString += c;
+			}
+		}
+		String postfixString = new InfixToPostfixConverter(reversedInfixString).convert();
 		String prefixString = new StringBuffer(postfixString).reverse().toString();
 		return prefixString;
 	}
